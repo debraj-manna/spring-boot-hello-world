8811b1eaf866ebcd79181fa0dcea459babf65223
Debraj Manna <debraj@spotnana.com>
2024-04-23 21:59:48 +0530

    Remove unused imports

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,10 +1,8 @@
 package com.example.helloworld;
 
 import com.example.helloworld.aspects.DaoExceptionHandler;
-import com.example.helloworld.config.ServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.retry.annotation.EnableRetry;
 
