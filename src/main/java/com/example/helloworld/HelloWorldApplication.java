690b31591dbbd40b1505f7aec7f28057711921e4
Alexander Kriegisch <Alexander@Kriegisch.name>
2024-04-23 18:29:41 +0200

    Fix aop.xml and application config
    - Do not use @Import, because it makes Spring treat the native
      AspectJ aspect like a Spring @Configuration class, falsely creating a
      special type of CGLIB proxy for it.
    
    - Make sure to set the correct aspect weaving scope
      com.example.helloworld..*, where the double-dot notation includes
      sub-packages, too.

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -1,16 +1,18 @@
 package com.example.helloworld;
 
-import com.example.helloworld.aspects.DaoExceptionHandler;
+import com.example.helloworld.controller.HelloWorldController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.retry.annotation.EnableRetry;
 
-@Import({DaoExceptionHandler.class})
+//@Import({ DaoExceptionHandler.class })
 @SpringBootApplication
 @EnableRetry
 public class HelloWorldApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
+  public static void main(String[] args) {
+    try (ConfigurableApplicationContext context = SpringApplication.run(HelloWorldApplication.class, args)) {
+      context.getBean(HelloWorldController.class).sendGreetings();
     }
+  }
 }
