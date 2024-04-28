074add44b8f59180fe152724895338ea547f2811
Debraj Manna <debraj@spotnana.com>
2024-04-28 23:42:59 +0530

    Fix null error

--- src/main/java/com/example/helloworld/aspects/DaoExceptionHandler.java
+++ src/main/java/com/example/helloworld/aspects/DaoExceptionHandler.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Aspect
--- src/main/java/com/example/helloworld/config/AspectConfig.java
+++ src/main/java/com/example/helloworld/config/AspectConfig.java
@@ -1,9 +1,11 @@
 package com.example.helloworld.config;
 
+import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 
 @Configuration
@@ -14,6 +16,6 @@ public class AspectConfig {
   @Bean
   public Map<Long, String> errorLogMap() {
     log.info("Initialising Bean");
-    return Map.of();
+    return new HashMap<>();
   }
 }
